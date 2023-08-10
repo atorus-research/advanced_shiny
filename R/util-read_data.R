a154be0e1f861607f6ce8d1a038a4a8b43c52bf0
Matt Roumaya <matt.roumaya@atorusresearch.com>
2023-08-10 19:49:12 +0000

    update read_data

--- R/util-read_data.R
+++ R/util-read_data.R
@@ -2,19 +2,35 @@
 #' 
 #' Function to read in data needed for application
 #' 
-#' @importFrom haven read_xpt
+#' @importFrom odbc odbc
+#' @importFrom DBI dbConnect dbDisconnect dbReadTable 
 #'
 #' @return a list of data.frames for use in the application
 #' @export
 #'
 #' @examples read_data()
 read_data <- function() {
+   
+   # hardcode connection string ----------------------------------------------
+   # -- this should probably be its own function
+   # -- user should be prompted for UID/PWD, or it should be inherited by Active Directory/etc.
+   con <- DBI::dbConnect(odbc::odbc(),
+                         Driver = "/opt/rstudio-drivers/mysql/bin/lib/libmysqlodbc_sb64.so",
+                         Server = "bd-dev-db.mysql.database.azure.com",
+                         database = "bd",
+                         UID = "atorusadmin",
+                         PWD = "job3l=efREDov!zopH*9",
+                         port = 3306,
+                         Trusted_Connection = "True")
    return(
       list(
-         adsl = haven::read_xpt(url("https://github.com/phuse-org/TestDataFactory/raw/main/Updated/TDF_ADaM/adsl.xpt")),
-         adae = haven::read_xpt(url("https://github.com/phuse-org/TestDataFactory/raw/main/Updated/TDF_ADaM/adae.xpt")),
-         adlb = haven::read_xpt(url("https://github.com/phuse-org/TestDataFactory/raw/main/Updated/TDF_ADaM/adlbc.xpt")),
-         advs = haven::read_xpt(url("https://github.com/phuse-org/TestDataFactory/raw/main/Updated/TDF_ADaM/advs.xpt"))
+         adsl = DBI::dbReadTable(conn = con, name = "phuse_original_adsl"),
+         adae = DBI::dbReadTable(conn = con, name = "phuse_original_adae"),
+         adlb = DBI::dbReadTable(conn = con, name = "phuse_original_adlb"),
+         advs = DBI::dbReadTable(conn = con, name = "phuse_original_advs")
       )
    )
+   
+   DBI::dbDisconnect(conn = con)
+   
 }
