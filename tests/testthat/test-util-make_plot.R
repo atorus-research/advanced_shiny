cbfaf421a7ebf73897146dc9cbd5d341f5e28245
Maya Gans <jaffe.maya@gmail.com>
2023-08-14 02:19:55 +0000

    adding test

--- tests/testthat/test-util-make_plot.R
+++ tests/testthat/test-util-make_plot.R
@@ -8,7 +8,7 @@ test_that("make_plot generates no plot without trta data", {
    expect_null(make_plot(data, NULL, unique(data$PARAM)))
 })
 
-test_that("make_plot generates a plot when supplied with trta and param data", {
+test_that("make_plot generates a ggplot2 object", {
    plotObj <- make_plot(data, unique(data$TRTA), unique(data$PARAM))
    expect_equal(class(plotObj), c("gg", "ggplot"))
 })
