46442b118462a879fe55d48e1eca858ae7b6848e
Maya Gans <jaffe.maya@gmail.com>
2023-08-23 19:34:35 +0000

    use plotly

--- R/mod-plot.R
+++ R/mod-plot.R
@@ -53,3 +53,68 @@ plotServer <- function(id, data) {
     }
   )
 }
+#' Plot Module UI
+#'
+#' @param id unique namespace of the plotting module
+#' 
+#' @importFrom plotly plotlyOutput renderPlotly
+#'
+#' @return a plot module with a title, subtitle, and plot
+#' @export
+plotUI <- function(id) {
+   ns <- NS(id)
+   nav_panel("Plot",
+             tags$div(class="left-margin",
+                      shiny::tags$div(
+                         class="custom-flexbox",
+                         controlsUI(ns("controls")),
+                         shiny::actionButton("print", "Print Plot")
+                      ),
+                      h1("Plot of Labs"),
+                      uiOutput(ns('subtitle'))
+             ),
+             plotlyOutput(ns("plot"))
+   )
+}
+
+#' Plot module server
+#' 
+#' controls subset the plot and change the subtitle of the module
+#'
+#' @param id matching unique namespace ID of the UI module
+#' @param data data used for plotting and controls
+#' 
+#' @export
+plotServer <- function(id, data) {
+   moduleServer(
+      id,
+      function(input, output, session) {
+         
+         ns <- session$ns
+         
+         controls <- controlsServer("controls", data)
+         
+         output$subtitle <- renderUI({
+            logger::log_info(sprintf("[%s] plot subtitle triggered", id))
+            tags$div(
+               class="subtitle",
+               paste0(
+                  "Averaged measurements of selected parameters by treatments: ",
+                  paste(controls$trta(), collapse = ", "),
+                  " over the course of the study."
+               )
+            )
+         })
+         
+         observe({
+            output$plot <- renderPlotly({
+               logger::log_info(sprintf("[%s] plot ggplot2 triggered", id))
+               make_plot(data()$adlb, controls$trta(), controls$param())
+            })
+         }) %>% bindEvent(
+            data()
+         )
+         
+      }
+   )
+}
--- R/util-make_plot.R
+++ R/util-make_plot.R
@@ -8,27 +8,29 @@
 #' @param trta treatments to average for each line over time
 #' @param param parameters to facet the plot by
 #' 
-#' @import ggplot2
 #' @importFrom dplyr filter group_by summarise
+#' @importFrom plotly ggplotly
+#' @import ggplot2
 #'
 #' @return a ggplot2 object to be used inside the app
 #' @export
 make_plot <- function(data, trta, param) {
-
-      if (!is.null(data) & !is.null(trta) & !is.null(param)) {
-         data <- data |>
-            filter(TRTA %in% trta) |>
-            filter(PARAM %in% param) |>
-            group_by(TRTA, PARAM, AVISITN) |>
-            summarise(AVAL = mean(AVAL), .groups = 'keep')
-
-         plot <- ggplot(data, aes(x = AVISITN, y = AVAL, group = TRTA, color = TRTA)) +
-            geom_point() +
-            geom_line() +
-            facet_wrap(.~PARAM, scales = "free_y")
-
-         suppressWarnings(print(plot))
-
-      }
-
+   
+   if (!is.null(data) & !is.null(trta) & !is.null(param)) {
+      data <- data |>
+         filter(TRTA %in% trta) |>
+         filter(PARAM %in% param) |>
+         group_by(TRTA, PARAM, AVISITN) |>
+         summarise(AVAL = mean(AVAL), .groups = 'keep')
+      
+      plot <- ggplot(data, aes(x = AVISITN, y = AVAL, group = TRTA, color = TRTA)) +
+         geom_point() +
+         geom_line() +
+         facet_wrap(.~PARAM, scales = "free_y")
+      
+      ggplotly(plot)
+      
+   }
+   
+   
 }
