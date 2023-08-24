1eeb20a4da16daa6e8b7f87be06ab8a9bff760d2
Maya Gans <jaffe.maya@gmail.com>
2023-08-24 23:18:22 +0000

    remove solutions

--- tests/testthat/feautre04-table_filters.R
+++ tests/testthat/feautre04-table_filters.R
@@ -13,60 +13,17 @@ testthat::describe("Feature 04: Table can be filtered by treatment and parameter
    table_obj <- jsonlite::fromJSON(driver_app$get_values()$output[['table-table']])
    
    it("a table is drawn with 5 columns and 18 grouped rows", {
-      expect_equal(length(table_obj$x$tag$attribs$columns$id), 5)
-      grouping <- table_obj$x$tag$attribs$groupBy
-      expect_equal(
-         length(unique(table_obj$x$tag$attribs$data[[grouping]])),
-         18
-      )
+      # CODE HERE
    })
    
    it("remove Placebo from the Treatments dropdown the table has 4 columns", {
-      
-      driver_app$set_inputs(
-         'table-controls-trta' = c("Xanomeline High Dose", "Xanomeline Low Dose")
-      )
-      
-      new_table_obj <- jsonlite::fromJSON(driver_app$get_values()$output[['table-table']])
-      
-      expect_equal(length(new_table_obj$x$tag$attribs$columns$id), 4)
-      
+      # CODE HERE
    })
    
    it("remove Sodium (mmol/L) from the Parameters dropdown,
       the table now has 17 grouped rows, 
       and no rowname with the text Sodium (mmol/L)", {
-         
-         driver_app$set_inputs(
-            'table-controls-param' = c(
-               "Alanine Aminotransferase (U/L)",
-               "Albumin (g/L)",               
-               "Alkaline Phosphatase (U/L)",
-               "Aspartate Aminotransferase (U/L)",
-               "Bilirubin (umol/L)",
-               "Blood Urea Nitrogen (mmol/L)" ,   
-               "Calcium (mmol/L)",
-               "Chloride (mmol/L)",              
-               "Cholesterol (mmol/L)",
-               "Creatine Kinase (U/L)",
-               "Creatinine (umol/L)",
-               "Gamma Glutamyl Transferase (U/L)",
-               "Glucose (mmol/L)","Phosphate (mmol/L)",            
-               "Potassium (mmol/L)","Protein (g/L)",
-               "Urate (umol/L)"
-            )
-         )
-         
-         new_table_obj <- jsonlite::fromJSON(driver_app$get_values()$output[['table-table']])
-         grouping <- new_table_obj$x$tag$attribs$groupBy
-         
-         expect_equal(
-            length(unique(new_table_obj$x$tag$attribs$data[[grouping]])),
-            17
-         )
-         
-         expect_true(!"Sodium (mmol/L)" %in% new_table_obj$x$tag$attribs$data[[grouping]])
-      
+      # CODE HERE
    })
    
    
--- tests/testthat/test-util-make_table.R
+++ tests/testthat/test-util-make_table.R
@@ -1,27 +1,22 @@
 data <- read_data()$adlb
 
 test_that("make_table generates no table without param data", {
-   expect_null(make_table(data, unique(data$TRTA), NULL))
+   # CODE HERE
 })
 
-
 test_that("make_plot generates no table without trta data", {
-   expect_null(make_table(data, NULL, unique(data$PARAM)))
+   # CODE HERE
 })
 
 test_that("make_plot generates a plotly object", {
-   table <- make_table(data, unique(data$TRTA), unique(data$PARAM))
-   expect_true("data.frame" %in% class(table))
+   # CODE HERE
 })
 
 test_that("make_table generates a data.frame with 5 columns", {
-   table <- make_table(data, unique(data$TRTA), unique(data$PARAM))
-   expect_equal(length(colnames(table)), 5)
+   # CODE HERE
 })
 
-
 test_that("make_table generates a data.frame with 216 rows", {
-   table <- make_table(data, unique(data$TRTA), unique(data$PARAM))
-   expect_equal(nrow(table), 216)
+   # CODE HERE
 })
 
