c5ddaaeb47ed66328f0adf6d61649f15583f766e
Maya Gans <jaffe.maya@gmail.com>
2023-08-24 23:15:57 +0000

    add unit tests to solution

--- tests/testthat/test-util-make_table.R
+++ tests/testthat/test-util-make_table.R
@@ -1,3 +1,27 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data <- read_data()$adlb
+
+test_that("make_table generates no table without param data", {
+   expect_null(make_table(data, unique(data$TRTA), NULL))
 })
+
+
+test_that("make_plot generates no table without trta data", {
+   expect_null(make_table(data, NULL, unique(data$PARAM)))
+})
+
+test_that("make_plot generates a plotly object", {
+   table <- make_table(data, unique(data$TRTA), unique(data$PARAM))
+   expect_true("data.frame" %in% class(table))
+})
+
+test_that("make_table generates a data.frame with 5 columns", {
+   table <- make_table(data, unique(data$TRTA), unique(data$PARAM))
+   expect_equal(length(colnames(table)), 5)
+})
+
+
+test_that("make_table generates a data.frame with 216 rows", {
+   table <- make_table(data, unique(data$TRTA), unique(data$PARAM))
+   expect_equal(nrow(table), 216)
+})
+
