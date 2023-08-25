59772a8a58cee2f8e562a210d7cce7e66da38845
Maya Gans <jaffe.maya@gmail.com>
2023-08-25 23:32:02 +0000

    changing to disp_plot

--- R/mod-plot.R
+++ R/mod-plot.R
@@ -1,61 +1,6 @@
 #' Plot Module UI
 #'
 #' @param id unique namespace of the plotting module
-#'
-#' @return a plot module with a title, subtitle, and plot
-#' @export
-plotUI <- function(id) {
-  ns <- NS(id)
-  nav_panel("Plot",
-            tags$div(class="left-margin",
-              shiny::tags$div(
-               class="custom-flexbox",
-                  controlsUI(ns("controls")),
-                  shiny::actionButton("print", "Print Plot")
-               ),
-               h1("Plot of Labs"),
-               uiOutput(ns('subtitle'))
-            ),
-            plotOutput(ns("plot"))
-  )
-}
-
-#' Plot module server
-#' 
-#' controls subset the plot and change the subtitle of the module
-#'
-#' @param id matching unique namespace ID of the UI module
-#' @param data data used for plotting and controls
-#' 
-#' @export
-plotServer <- function(id, data) {
-  moduleServer(
-    id,
-    function(input, output, session) {
-
-       controls <- controlsServer("controls", data)
-
-       output$subtitle <- renderUI({
-          tags$div(
-             class="subtitle",
-             paste0(
-                "Averaged measurements of selected parameters by treatments: ",
-                paste(controls$trta(), collapse = ", "),
-                " over the course of the study."
-             )
-          )
-       })
-
-       output$plot <- renderPlot({
-          make_plot(data()$adlb, controls$trta(), controls$param())
-       })
-
-    }
-  )
-}
-#' Plot Module UI
-#'
-#' @param id unique namespace of the plotting module
 #' 
 #' @importFrom plotly plotlyOutput renderPlotly
 #'
@@ -73,7 +18,7 @@ plotUI <- function(id) {
                       h1("Plot of Labs"),
                       uiOutput(ns('subtitle'))
              ),
-             plotlyOutput(ns("plot"))
+             plotlyOutput(ns("disp_plot"))
    )
 }
 
@@ -95,7 +40,6 @@ plotServer <- function(id, data) {
          controls <- controlsServer("controls", data)
          
          output$subtitle <- renderUI({
-            logger::log_info(sprintf("[%s] plot subtitle triggered", id))
             tags$div(
                class="subtitle",
                paste0(
@@ -107,8 +51,7 @@ plotServer <- function(id, data) {
          })
          
          observe({
-            output$plot <- renderPlotly({
-               logger::log_info(sprintf("[%s] plot ggplot2 triggered", id))
+            output$disp_plot <- renderPlotly({
                make_plot(data()$adlb, controls$trta(), controls$param())
             })
          }) %>% bindEvent(
