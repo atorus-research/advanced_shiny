494cab78d789955528f1c67fa7f97d89f0e1f8f9
Maya Gans <jaffe.maya@gmail.com>
2023-08-26 00:20:15 +0000

    enhance table function

--- make_table.R
+++ make_table.R
@@ -3,6 +3,7 @@ library(dplyr)
 
 make_table <- function(data, trta, param) {
    
+   
    if (!is.null(data) & !is.null(trta) & !is.null(param)) {
       data <- data |>
          filter(TRTA %in% trta) |>
@@ -12,7 +13,13 @@ make_table <- function(data, trta, param) {
          add_layer(
             group_count(AVISIT, PARAM)
          ) |>
-         build()
+         build() |>
+         select(-c(starts_with('ord'))) |>
+         rename(
+            Parameter = row_label1,
+            Week = row_label2,
+         ) |>
+         rename_at(vars(starts_with('var1_')), list(~sub('var1_', '', .)))
    }
    
 }
