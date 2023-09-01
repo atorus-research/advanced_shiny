fb8d37eb47bac1fad888ef1aee03b1eee804bbc3
Martin Frigaard <mjfrigaard@pm.me>
2023-09-01 14:33:15 -0700

    clean up/align standalone app function

--- R/make_plot.R
+++ R/make_plot.R
@@ -20,7 +20,8 @@ make_plot <- function(data, trta, param) {
          dplyr::group_by(TRTA, PARAM, AVISITN) |>
          dplyr::summarise(AVAL = mean(AVAL), .groups = 'keep')
       
-      plot <- ggplot2::ggplot(data, ggplot2::aes(x = AVISITN, y = AVAL, group = TRTA, color = TRTA)) +
+      plot <- ggplot2::ggplot(data, 
+         ggplot2::aes(x = AVISITN, y = AVAL, group = TRTA, color = TRTA)) +
          ggplot2::geom_point() +
          ggplot2::geom_line() +
          ggplot2::facet_wrap(.~PARAM, scales = "free_y")
